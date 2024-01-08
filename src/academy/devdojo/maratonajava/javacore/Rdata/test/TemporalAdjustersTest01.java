e9dc338ce694d0cc5f42ca4813b4e3fe5b758e5d
Mr.Nit <contato.mr.nit@gmail.com>
2024-01-08 13:18:56 -0300

    Atualização de arquivos 04/01/2024

---
+++ src/academy/devdojo/maratonajava/javacore/Rdata/test/TemporalAdjustersTest01.java
@@ -0,0 +1,34 @@
+package academy.devdojo.maratonajava.javacore.Rdata.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = now.plusDays(2);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = now.withDayOfMonth(19);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.THURSDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.THURSDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.previous(DayOfWeek.THURSDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+    }
+}
