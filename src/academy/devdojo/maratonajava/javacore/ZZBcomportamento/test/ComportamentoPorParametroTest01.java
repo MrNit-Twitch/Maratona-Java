fa1226baabdee336324c7ef9e896b50c8b2fcbae
Mr.Nit <contato.mr.nit@gmail.com>
2024-05-29 16:25:48 -0300

    Atualização de arquivos 29/05/2024

--- src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/test/ComportamentoPorParametroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/test/ComportamentoPorParametroTest01.java
@@ -9,16 +9,53 @@ public class ComportamentoPorParametroTest01 {
     private static List<Car> cars = List.of(new Car("green", 2011), new Car("black", 1998), new Car("red", 2019));
 
     public static void main(String[] args) {
+
         System.out.println(filterGreenCar(cars));
+        System.out.println(filterRedCar(cars));
+        System.out.println(filterCarByColor(cars, "green"));
+        System.out.println(filterCarByColor(cars, "black"));
+        System.out.println("--------");
+        System.out.println(filterByYearBefore(cars,2015));
+
     }
 
     private static List<Car> filterGreenCar(List<Car> cars) {
-        List<Car> greenCars = new ArrayList<>();
+        List<Car> filteredCar = new ArrayList<>();
         for (Car car : cars) {
             if (car.getColor().equals("green")) {
+                filteredCar.add(car);
+            }
+        }
+        return filteredCar;
+    }
+
+    private static List<Car> filterRedCar(List<Car> cars) {
+        List<Car> filteredCar = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getColor().equals("red")) {
+                filteredCar.add(car);
+            }
+        }
+        return filteredCar;
+    }
+
+    private static List<Car> filterCarByColor(List<Car> cars, String cor) {
+        List<Car> greenCars = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getColor().equals(cor)) {
                 greenCars.add(car);
             }
         }
         return greenCars;
+    }
+
+    private static List<Car> filterByYearBefore(List<Car> cars, int year) {
+        List<Car> filteredCar = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getYear() < year) {
+                filteredCar.add(car);
+            }
+        }
+        return filteredCar;
     }
 }
